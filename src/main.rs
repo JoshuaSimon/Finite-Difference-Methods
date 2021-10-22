27215875fd189595ec721ee6d8b0c66907c55305
Joshua Simon <39858834+JoshuaSimon@users.noreply.github.com>
2021-10-22 21:21:31 +0200

    Added solve function.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ndarray::{Array, Array1, Array2, Axis, s};
-use ndarray::prelude::*;
+//use ndarray::prelude::*;
 use ndarray_linalg::Solve;
+//use nalgebra;
 
 struct Diffusion {
     alpha: f32,                     // thermal diffusivity, positive constant
@@ -14,7 +15,7 @@ struct Diffusion {
 }
 
 impl Diffusion {
-    // Constructor
+    /// Constructor
     fn new(alpha: f32, delta_x: f32, delta_t: f32, n_x: usize, n_t: usize) -> Diffusion {
         Diffusion {
             alpha: alpha,
@@ -22,26 +23,29 @@ impl Diffusion {
             delta_t: delta_t,
             n_x: n_x,
             n_t: n_t,
-            r: alpha * delta_t /((delta_x * delta_x) * 2.0), // How to do delta_x^2?
+            r: alpha * delta_t  / (delta_x.powi(2) * 2.0),
             a_matrix: Array::eye(n_x),
             b_vector: Array::zeros(n_x),
         }
     }
 
-    // Assemble the b-Vector and set bounary conditions
-    // Takes the temperature vector form the previous iteration
-    // as input.
+    /// Assemble the b-Vector and set bounary conditions
+    /// Takes the temperature vector form the previous iteration
+    /// as input.
     fn assemble_b_vector(&mut self, temp: Array1<f32>) {
         for i in 1..self.n_x - 1 {
             self.b_vector[[i]] = self.r * temp[i - 1] + (1.0 - 2.0 * self.r) * temp[i] + self.r * temp [i + 1];
         }
 
         // Is this correct?
-        self.b_vector[[0]] = self.b_vector[[0]] + self.r * temp[0];
-        self.b_vector[[self.n_x - 1]] = self.b_vector[[self.n_x - 1]] + self.r * temp[self.n_x - 1];
+        //self.b_vector[[0]] = self.b_vector[[0]] + self.r * temp[0];
+        //self.b_vector[[self.n_x - 1]] = self.b_vector[[self.n_x - 1]] + self.r * temp[self.n_x - 1];
+
+        self.b_vector[[0]] = 50.0;
+        self.b_vector[[self.n_x - 1]] = 80.0;
     }
 
-    // Construct system matrix add boundary conditions
+    /// Construct system matrix add boundary conditions
     fn add_boundaries(&mut self, t_ambience: f32, t_0: f32, t_n: f32) {
         // Assemble temperature vector from start values.
         //let mut temp: Vec<f32> = vec![t_ambience; self.n_x]; 
@@ -73,24 +77,30 @@ impl Diffusion {
         self.a_matrix[[self.n_x - 1, self.n_x - 1]] = 1.0;
 
         // Assemble the b-Vector and set bounary conditions
-        self.assemble_b_vector(temp);
+        //self.assemble_b_vector(temp);
+        self.b_vector = temp;
     }
 
     // Solve the matrix equation A * z = b for the time discretization
     fn solve(&mut self) {
+        println!("Temprature on t = {}: {:?}", "start", self.b_vector);
         for t in 0..self.n_t {
             let b = self.b_vector.clone();
             let z = self.a_matrix.solve_into(b).unwrap();
             self.assemble_b_vector(z);
+
+            if t % 10 == 0 {
+                println!("Temprature on t = {}: {:?}", t, self.b_vector);
+            }
         }
     }
 }
 
 
 fn main() {
-    let mut model = Diffusion::new(1.0, 0.5, 0.5, 10, 2);
+    let mut model = Diffusion::new(1.0, 0.5, 0.5, 10, 100);
     model.add_boundaries(20.0, 50.0, 80.0);
-    //model.solve();
+    model.solve();
 
     println!("A-Matrix: {:?}", model.a_matrix);
     println!("b-Vector: {:?}", model.b_vector);
